acaf7ed2c5202af265650b758dae18d29a76f4ca
Sara Meira Moutta <smmoutta@uesc.br>
2026-03-10 13:18:51 -0300

    Update run.py

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 from src.sectorization_algorithm import run_sectorization
 
-#Para cenário com 20 bairros
+#For a scenario with 20 neighborhoods
 from data.scenario_20_districts import V, A, C
 
-#Para cenário com 30 bairros
+#For a scenario with 30 neighborhoods
 #from data.scenario_30_districts import V, A, C 
+
 
 run_sectorization(V, A, C)
